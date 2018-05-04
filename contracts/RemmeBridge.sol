3bd6eafb05a8889009e372f8a16a73170eeb9fb3
Koroqe <koroqe@gmail.com>
2018-05-04 15:53:26 +0300

    Add expireSwap() small fixes

--- contracts/RemmeBridge.sol
+++ contracts/RemmeBridge.sol
@@ -40,15 +40,16 @@ contract RemmeBridge {
 
     //EVENTS
     event OpenSwap(uint256 id);
+    event ExpireSwap(uint256 id);
 
     //MODIFIERS
     modifier onlyExpiredSwap(uint256 _swapId) {
-        require (block.number >= swaps[_swapId].timelock);
+        require (now >= swaps[_swapId].timelock);
         _;
     }
 
     modifier onlyNotExpiredSwap(uint256 _swapId) {
-        require (block.number < swaps[_swapId].timelock);
+        require (now < swaps[_swapId].timelock);
         _;
     }
 
@@ -62,26 +63,21 @@ contract RemmeBridge {
     * @title OpenSwap
 	* @notice User should use this function for request swap locking his tokens
 	* @dev There two cases of using this function:
-	* 1) Alice requests swap and don't put _secretLock argument
+	* 1) Alice requests swap and don't put _secretLock argument, and should send ether
+	*    required by atomicSwapProvider for gas coverage
 	* 2) Bob opens swap after Alice's request in Remmechain
-	* @param purchaser who paid for the tokens
-    * @param beneficiary who got the tokens
+	* @param will be set only by Bob
     */
-    function openSwap ( //user should send some ether for gas coverage
+    function openSwap (
         address _receiverAddress,
-        bytes _secretLock, //will be set only by Bob
+        bytes _secretLock,
         uint256 _amount,
         bytes32 _remchainAddress,
         bytes _emailAddressEncryptedOptional)
     payable
     {
-        //
-        if(_secretLock == bytes(0)) {
-            uint256 _timelock = block.number + BLOCKS24; // in case when user request eth-rem swap (Alice)
-			//todo check msg.value for gas coverage
-        } else {
-            uint256 _timelock = block.number + BLOCKS48; // in case when user set secret lock (Bob)
-        }
+        //set timelock 24h in case user request eth-rem swap (Alice) otherwise (Bob) set 48h
+        uint256 _timelock = (_secretLock == bytes(0)) ? now + LOCK24 : now + LOCK48;
 
         //create and save new swap
 	    AtomicSwap memory swap = Swap({
@@ -99,16 +95,35 @@ contract RemmeBridge {
         swapStates[swap.swapId] = States.OPENED;
 
         //deposit tokens
-		require(REMToken.transferFrom(msg.sender, address(this), _amount);
+		require(REMToken.transferFrom(msg.sender, address(this), _amount));
+        deposits[msg.sender] = amount;
 
-        //transfer ether to atomicSwapProvider
-		if (msg.value != 0) {
-			atomicSwapProvider.transfer(msg.value);
-		}
+        //transfer ether to atomicSwapProvider (if required)
+		if (msg.value != 0) atomicSwapProvider.transfer(msg.value);
 
+        //set state
+        swapStates[swap.swapId] = States.OPENED;
         emit OpenSwap(swap.swapId);
     }
 
-    
+    function expire(uint256 _swapId) onlyExpiredSwap(_swapId) {
+        //check that swap opened by msg.sender
+        require(msg.sender == swaps[swapId].senderAddress);
+        //check that swap still opened
+        require(swapStates[_swapId] = States.OPENED);
+        //check that msg.sender has enough funds in deposits
+        require(deposits[msg.sender] >= swaps[_swapId].amount);
+
+        AtomicSwap currentSwap = swaps[_swapId];
+        uint256 memory lockedTokens = currentSwap.amount;
+
+        //withdraw funds
+        deposits[msg.sender] = deposits[msg.sender].sub(lockedTokens);
+        REMToken.transfer(msg.sender, amount);
+
+        //set state
+        swapStates[_swapId] = States.EXPIRED;
+        emit ExpireSwap(_swapId);
+    }
 
 }
